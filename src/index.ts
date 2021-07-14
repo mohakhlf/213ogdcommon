7b4402066b70397bb90ec37985a517fe7eb98621
Mohammed <Mohammed.K@itdm-gitlab.ssii.be>
2021-07-14 21:32:19 +0200

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
 export * from './middlewares/authMiddleware';
 export * from './middlewares/fileMiddleware';
+const {
+  addGatewayDataSourceToSubscriptionContext
+} = require("./utils/subscriptions");
+const { GatewayDataSource } = require("./datasources/GatewayDataSource");
+const {
+  getGatewayApolloConfig,
+  makeSubscriptionSchema
+} = require("./utils/schema");
+
+module.exports = {
+  addGatewayDataSourceToSubscriptionContext,
+  GatewayDataSource,
+  getGatewayApolloConfig,
+  makeSubscriptionSchema
+};
